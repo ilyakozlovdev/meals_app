48206844d9392003929e0c6b9d6446bcaba826c3
Ilya Kozlov <ilya@kozlov.dev>
2020-07-14 22:48:54 +0600

    added tabs_screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../screens/favourites_screen.dart';
+import '../screens/categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavouritesScreen(), 'title': 'Favourites'}
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Center(
+          child: Text('Drawer'),
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), title: Text('Categories')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star), title: Text('Favourites'))
+        ],
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+      ),
+    );
+  }
+}
